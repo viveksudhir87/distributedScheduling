c14780c6db3fda3ec3b804b590e07ec3964339a4
viveksudhir87 <viveksudhir87@gmail.com>
2024-06-03 16:12:15 +0530

    initial commit

---
+++ src/main/java/com/vivek/learning/distributedScheduling/DistributedSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.learning.distributedScheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedSchedulingApplication.class, args);
+	}
+
+}
